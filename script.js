33fef27b71c3b9c0bdf1f3c57fba5948d858410c
ChristofferRodGreffel <119002008+ChristofferRodGreffel@users.noreply.github.com>
2023-05-25 13:24:12 +0200

    Update hestene
    - Added header and footer to hestene

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ const header = document.querySelector("header");
 
 let prevScrollpos = window.pageYOffset;
 window.onscroll = function () {
-  var currentScrollPos = window.pageYOffset;
+  let currentScrollPos = window.pageYOffset;
   if (currentScrollPos > 500) {
     if (prevScrollpos > currentScrollPos) {
       header.style.top = "0";
@@ -92,44 +92,3 @@ burger.addEventListener("click", () => {
     });
   }
 });
-
-// HOLD SIDE FILTRE KNAP
-
-// const filterKnap = document.querySelector("filter-knap");
-// const filterMenu = document.querySelector("filter-menu");
-// const filterCategory = document.querySelectorAll(".filter-category");
-
-// filterCategory.forEach((filterCat) => {
-//   const filterCatContent = filterCat.querySelector(".filter-cat-content");
-//   filterCatContent.addEventListener("click", (el) => {
-//     el.stopPropagation();
-//   });
-//   filterCat.addEventListener("click", (el) => {
-//     let catHeight = filterCatContent.scrollHeight;
-//     let currentHeight = filterCatContent.clientHeight;
-//     if (currentHeight == 0) {
-//       filterCatContent.style.maxHeight = `${catHeight}px`;
-//     } else {
-//       filterCatContent.style.maxHeight = `0px`;
-//     }
-//     let currentIcon = catIcon.getAttribute("class");
-//     if (currentIcon == "fa-solid fa-plus") {
-//       catIcon.setAttribute("class", "fa-solid fa-minus");
-//     } else {
-//       catIcon.setAttribute("class", "fa-solid fa-plus");
-//     }
-//   });
-// });
-
-// let filterisOpen = false;
-
-// filterKnap.addEventListener("click", () => {
-//   if (!isOpen) {
-//     filterMenu.style.transform = "translateY(0px)";
-//     isOpen = true;
-//   } else {
-//     let filterMenuHeight = filterMenu.clientHeight;
-//     filterMenu.style.transform = `translateY(-${filterMenuHeight}px)`;
-//     isOpen = false;
-//   }
-// });
